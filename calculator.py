1140af293172e9e0ceafa597ef7a8d53ac50bb7f
Joe Nibali <joenibali27@gmail.com>
2021-06-06 12:58:39 -0700

    added more tests

--- calculator.py
+++ calculator.py
@@ -9,3 +9,11 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+
+def divide(a, b):
+    return a / b
+
+
+def multiplay(a, b):
+    return a * b
--- test.py
+++ test.py
@@ -10,4 +10,10 @@ class TestCalculatorApp:
         
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
+
+    def test_divide(self):
+        assert 2 == calculator.divide(10, 5)
+    
+    def test_multiply(self):
+        assert 50 == calculator.divide(10, 5)
     
